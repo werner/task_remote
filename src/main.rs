20a551d435933d10763078b1053a9d1f3c203bed
Werner Echezuria <werner_a_e@yahoo.es>
2018-01-22 19:56:09 -0300

    add line numbers to code box

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use language_chooser::{LanguageChooser};
 use source_view::{SourceView};
 use gio::prelude::*;
 use gtk::prelude::*;
-use sourceview::{View};
+use sourceview::{View, ViewExt};
 
 use std::env::args;
 
@@ -30,17 +30,27 @@ fn build_ui(application: &gtk::Application) {
     }));
 
     let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 2);
-    let vbox_scripts = gtk::Box::new(gtk::Orientation::Vertical, 3);
+    let vbox_scripts = gtk::Box::new(gtk::Orientation::Vertical, 4);
+
+    let title = gtk::Entry::new();
+    title.set_placeholder_text("Title");
+    vbox_scripts.pack_start(&title, false, false, 5);
 
     let pre_hook = gtk::Entry::new();
+    pre_hook.set_placeholder_text("Pre hook");
     vbox_scripts.pack_start(&pre_hook, false, false, 5);
 
     let source_view = SourceView::new();
 
     let view = View::new_with_buffer(&source_view.buffer);
+    view.has_focus();
+    view.grab_focus();
+    view.set_show_line_numbers(true);
+    view.set_auto_indent(true);
     vbox_scripts.pack_start(&view, true, true, 5);
 
     let post_hook = gtk::Entry::new();
+    post_hook.set_placeholder_text("Post hook");
     vbox_scripts.pack_start(&post_hook, false, false, 5);
 
     hbox.pack_start(&vbox_scripts, true, true, 1);
