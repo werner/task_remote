0120b86d0f9767834030bd8edbc85a4ebc9d7e47
Werner Echezuria <werner_a_e@yahoo.es>
2018-01-22 10:53:48 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+extern crate gio;
+extern crate gtk;
+extern crate sourceview;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+use sourceview::*;
+
+use std::env::args;
+
+// make moving clones into closures more convenient
+macro_rules! clone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
+        }
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(clone!(@param $p),)+| $body
+        }
+    );
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("First GTK+ Program");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    window.connect_delete_event(clone!(window => move |_, _| {
+        window.destroy();
+        Inhibit(false)
+    }));
+
+    let button = gtk::Button::new_with_label("Click me!");
+
+    window.add(&button);
+
+    window.show_all();
+}
+
+fn main() {
+    let application = gtk::Application::new("com.github.basic",
+                                            gio::ApplicationFlags::empty())
+                                       .expect("Initialization failed...");
+
+    application.connect_startup(|app| {
+        build_ui(app);
+    });
+    application.connect_activate(|_| {});
+
+    application.run(&args().collect::<Vec<_>>());
+}
