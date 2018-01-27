7e99cb75e61b2883f534716c4b55b75f344bae9d
Werner Echezuria <werner_a_e@yahoo.es>
2018-01-27 16:00:56 -0300

    add php to languages

--- migrations/2018-01-24-111244_create_languages/up.sql
+++ migrations/2018-01-24-111244_create_languages/up.sql
@@ -10,3 +10,4 @@ INSERT INTO languages(name, value) VALUES ("Ruby", "ruby");
 INSERT INTO languages(name, value) VALUES ("Python", "python");
 INSERT INTO languages(name, value) VALUES ("Perl", "perl");
 INSERT INTO languages(name, value) VALUES ("Javascript", "js");
+INSERT INTO languages(name, value) VALUES ("PHP", "php");
